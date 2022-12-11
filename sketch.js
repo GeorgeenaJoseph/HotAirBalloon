a3150df6565ad7fc9303b244a51aec3e33711f20
GeorgeenaJoseph <74125285+GeorgeenaJoseph@users.noreply.github.com>
2022-12-11 14:55:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+var balloon, balloonImg
+var obstacleTop
+var obstacleBottom, obstacleBottom1, obstacleBottom2, obstacleBottom1
+var gameOver, gameOverImg
+var restart, restartImg
+var backgroundImg
+
+var score = 0;
+
+//game states      
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+
+function preload(){
+
+}
+
+function setup(){
+
+  createCanvas(400,400)
+
+
+}
+
+function draw() {
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
